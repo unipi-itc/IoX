479569a9e4fc7a7300344b23004eaf27c2487a6a
Andrea Canciani <ranma42@gmail.com>
2016-07-06 11:35:14 +0200

    New `Module` implementation and interface

--- IoX.Modules/modules.fs
+++ IoX.Modules/modules.fs
@@ -1,155 +1,70 @@
-﻿namespace IoX.Modules
+namespace IoX.Modules
 
+open EvReact
+open EvReact.Expr
+open IoX.Suave
 open Suave
 open Suave.Operators
-open Suave.Files
-open IoX.Suave
 open Suave.EvReact
-open EvReact
+open System
 open System.Net
-open IoX.Json
 
-[<AbstractClass>]
-type Module(name:string, title:string, description:string) =
-  let configurationName = "module.conf"
-  let verbs = WebPartArray()
-  let isMain = name = Module.ROOT
-  do
-    if name = null ||
-       name = "" ||
-       (not(isMain) && System.Text.RegularExpressions.Regex.IsMatch(name, "[^_a-zA-Z\\-0-9]")) then
-      failwith "Invalid module name"
+[<AttributeUsage(AttributeTargets.Class, Inherited=false)>]
+type ModuleAttribute() =
+  inherit Attribute()
+  member val Name = "IoX module" with get, set
+  member val Description = "Description missing" with get, set
 
-  member this.Name = name
-  member this.Title = title
-  member this.Description = description
+type IModuleData<'C> =
+  abstract member Id: string
+  abstract member BaseUri: System.Uri
+  abstract member Path: string
+  abstract member Configuration: IoX.Utils.ConfigurationFile<'C>
+  // TODO: abstract member Logger: Logging.Logger
 
-  member val Browsable = false with get, set
+type Module() =
+  let verbs = WebPartDispatcher("relPath")
+  let genericVerbs = WebPartArray()
 
-  member this.ModuleStaticFilesPath
-    with get() =
-      let sep = System.IO.Path.DirectorySeparatorChar
-      if isMain then
-        Module.HomePath
-      else
-        sprintf "%s%cstatic" this.ModuleFolder sep
-
-  member this.ModuleFolder
-    with get() =
-      let sep = System.IO.Path.DirectorySeparatorChar
-      if isMain then
-        Module.HomePath
-      else
-        sprintf "%s%c%s" Module.ModulesPath sep name
-
-  member this.DefaultConfigurationFileFullName =
-    let sep = System.IO.Path.DirectorySeparatorChar
-    sprintf "%s%c%s" this.ModuleFolder sep configurationName
-
-  member this.Verbs = verbs
-
-  member this.Handle query =
-    let r = RequestErrors.FORBIDDEN "Query not supported"
-    let r = if this.Browsable then browseFile this.ModuleStaticFilesPath query <|> r else r
-    this.Verbs.choose() <|> r
-
-  member this.ModuleFilter
-    with get() = verbs.Filter
-    and set(v) = verbs.Filter <- v 
-
-  member this.RegisterReplyEvent<'T>(pattern:string, ?msTimeout:int) =
-    let ewp, evt:WebPart*IEvent<MsgRequestEventArgs<'T>> = msgResponse(msTimeout)
-    let wp = regex pattern >=> ewp
-    verbs.Add(wp)
+  member val WebPart = verbs.WebPart <|> genericVerbs.WebPart
+  member internal this.Register(wp) = genericVerbs.Add(wp)
+  member internal this.Register(path, wp) = verbs.Add(path, wp)
+  member internal this.RegisterEvent(path, (wp, evt)) =
+    this.Register(path, wp)
     evt
 
-  member this.RegisterEvent<'T>(pattern:string) =
-    let ewp, evt:WebPart*IEvent<MsgContext*'T> = msgReact()
-    let wp = regex pattern >=> ewp
-    verbs.Add(wp)
-    evt
-  
-  member this.CreateRemoteTrigger<'T>(uri) =
-    createRemoteTrigger<'T>(defaultSendJson uri)
-
-  member this.ActivateNet<'T> (net:Expr<'T>) =
-    let orchestrator : Orchestrator<'T> = Orchestrator.create()
+  member this.ActivateNet net =
+    let orchestrator = Orchestrator.create()
     Utils.start0 orchestrator net
 
-  abstract OnLoad : unit -> unit
+  member this.RegisterReplyEvent(path, ?msTimeout) = this.RegisterEvent(path, msgResponse msTimeout)
+  member this.RegisterEvent(path) = this.RegisterEvent(path, msgReact ())
 
-  static member ROOT = "//"
+  member this.Root with set wp = this.Register("", wp)
+  member val Browsable = false with get, set
 
-  static member HomePath =
-    let sep = System.IO.Path.DirectorySeparatorChar
-    sprintf @"%s%c%s" System.Environment.CurrentDirectory sep "Home"
+  member this.SendJsonMessage (dest: Uri) x =
+    let json = Newtonsoft.Json.JsonConvert.SerializeObject(x)
+    let data = System.Text.Encoding.UTF8.GetBytes(json)
+    use client = new WebClient()
+    client.Headers.[HttpRequestHeader.ContentType] <- "application/json"
+    client.UploadData(dest, data)
 
-  static member ModulesPath =
-    let sep = System.IO.Path.DirectorySeparatorChar
-    sprintf @"%s%c%s" System.Environment.CurrentDirectory sep "Modules"
+  member this.SendREST dest data =
+    let entryToString (KeyValue(k,v)) =
+      let ek = Uri.EscapeDataString(k)
+      match v with
+      | None -> ek
+      | Some v ->
+        let ev = Uri.EscapeDataString(v)
+        sprintf "%s=%s" ek ev
 
-[<AbstractClass>]
-type DriverModule(name:string, title:string, description:string) =
-  inherit Module(name, title, description)
-  let isMain = name = Module.ROOT
-  do
-    if name = null || 
-       (not(isMain) 
-        && (System.Text.RegularExpressions.Regex.IsMatch(name, "[^a-zA-Z\\-0-9]") 
-            || name = System.String.Empty)) then
-      failwith "Invalid module name"
+    let args = Seq.map entryToString data |> String.concat "&"
+    use c = new WebClient()
+    c.DownloadString(Uri(dest, "?" + args))
 
-  member this.RegisterHttpEvent (pattern:string, ?filter:WebPart, ?msTimeout:int) = 
-    let wp, evt = httpResponse(msTimeout)
-    let rxwp = regex pattern >=> wp
-    let rwp = match filter with Some f -> f >=> rxwp | None -> rxwp
-    this.Verbs.Add(rwp)
-    evt
-  
-  member this.SendJsonMessage (data:Json, dest:System.Uri) =
-    async {
-      use c = new WebClient()
-      c.Headers.Add("content-type", "application/json")
-      let! ret = Async.AwaitTask(c.UploadStringTaskAsync(dest, (data |> toJson).ToString()))
-      return ret
-    }
+type DriverModule() =
+  inherit Module()
 
-  member this.SendREST(data:System.Collections.Generic.IDictionary<string, string option>, dest:System.Uri) =
-    async {
-      use c = new WebClient()
-      let txt = System.Text.StringBuilder()
-      txt.Append("?") |> ignore
-      let mutable first = true
-      for kv in data do
-        let ek = System.Uri.EscapeDataString(kv.Key)
-        if first then first <- false
-        else txt.Append('&') |> ignore
-        match kv.Value with
-        | Some v -> 
-          let ev = System.Uri.EscapeDataString(v)
-          txt.AppendFormat("{0}={1}", ek, ev) |> ignore
-        | None ->
-          txt.Append(ek) |> ignore
-      let u = System.Uri(sprintf "%s%s" dest.OriginalString (txt.ToString()))
-      let! ret = c.AsyncDownloadString(u)
-      return ret
-    }
-
-module Conf =
-  type ModuleDescriptor = { Name:string; Title:string; Assembly:string; Description:string; Summary: string }
-
-  let DefaultDescriptor = { Name = null; Title = null; Assembly = null; Description = ""; Summary = ""}
-
-  type ConfigurationFile<'T>(fn:string, def:'T) =
-    let mutable conf = def
-
-    member this.Configuration
-      with get() = conf
-      and set(v) = conf <- v
-
-    member this.Load() =
-      if System.IO.File.Exists(fn) then
-        conf <- IoX.Utils.readConfigurationFile(fn)
-
-    member this.Save() =
-      IoX.Utils.writeConfigurationFile fn conf
+  member this.RegisterHttpEvent(path, ?msTimeout) = this.RegisterEvent(path, httpResponse msTimeout)
+  member this.RegisterGenericWebPart(wp) = this.Register(wp)
--- IoX.Modules/suave.fs
+++ IoX.Modules/suave.fs
@@ -1,79 +1,33 @@
 ﻿namespace IoX
 
-open Suave
-open Suave.Filters
-open Suave.WebPart
-open Suave.Operators
-open Newtonsoft.Json.Linq
-open System.Text.RegularExpressions
-
-module Json =
-  let parseJson (s:string) =
-    JToken.Parse(s)
-
-  type Json =
-  | JObj of Json seq
-  | JProp of string * Json
-  | JArr of Json seq
-  | JVal of obj
-
-  let (!?) (o : obj) = JVal o
-
-  let rec toJson = function
-  | JVal v -> new JValue(v) :> JToken
-  | JProp(name, (JProp(_) as v)) -> new JProperty(name, new JObject(toJson v)) :> JToken
-  | JProp(name, v) -> new JProperty(name, toJson v) :> JToken
-  | JArr items -> new JArray(items |> Seq.map toJson) :> JToken
-  | JObj props -> new JObject(props |> Seq.map toJson) :> JToken
-
-
 module Suave =
-  let regexResponseContentFilter (regs:Regex array) =
-    fun (arg:HttpContext) ->
-      async {
-        let txt = arg.response.content.ToString()
-        match (regs |> Array.tryFind(fun re -> re.IsMatch(txt))) with
-        | Some _ -> return None
-        | None -> return Some arg
-      }
+  open Suave
+  open System.Text.RegularExpressions
 
-  let regex (pattern:string) (arg:HttpContext) =
-    async {
-      if Regex.IsMatch(arg.request.url.AbsolutePath, pattern) then
-        return Some arg
+  let regex pattern : WebPart =
+    let rx = Regex(pattern, RegexOptions.Compiled)
+    fun ctx ->
+      if rx.IsMatch(ctx.request.url.AbsolutePath) then
+        succeed ctx
       else
-        return None
-    }
+        fail
 
-  type WebPartArray<'a>() =
-    let webparts = ResizeArray<WebPart<'a>>()
-    let mutable filter : WebPart<_> option = None
+  type WebPartDispatcher(keyId) =
+    let webparts = System.Collections.Generic.Dictionary<_,_>()
 
-    member this.Filter
-      with get() = filter
-      and set(v) = filter <- v
+    member this.Add(k, wp) = webparts.Add(k, wp)
+    member val WebPart : WebPart =
+      fun ctx ->
+        match webparts.TryGetValue(ctx.userState.[keyId]) with
+        | (false, _) -> fail
+        | (true,wp) -> wp ctx
 
-    member this.Add(wp:WebPart<'a>) = webparts.Add(wp)
-    member this.Remove(wp:WebPart<'a>) = webparts.Remove(wp)
-    member this.Clear () = webparts.Clear()
+  type WebPartArray() =
+    let webparts = System.Collections.Generic.List<WebPart>()
 
-    member this.GetEnumerator() = webparts.GetEnumerator()
-
-    member this.Count with get() = webparts.Count
-
-    member this.Item with get(idx) = webparts.[idx]
-
-    member this.choose () =
-      let rec intchoose (idx) =
-        fun arg -> async {
-          if idx = webparts.Count then return None
-          else
-            let wp = webparts.[idx]
-            let! res = wp arg
-            match res with
-            | Some x -> return Some x
-            | None -> return! intchoose (idx + 1) arg
-        }
-      match filter with
-      | Some wp -> wp >=> (intchoose 0)
-      | None    -> intchoose 0
+    member this.Add(wp) = webparts.Add(wp)
+    member val WebPart : WebPart =
+      fun ctx ->
+        webparts
+        |> Seq.tryPick (fun wp -> Async.RunSynchronously (wp ctx))
+        |> async.Return
